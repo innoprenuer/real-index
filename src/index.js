4f0cd8453f2b2bcfd60496053c330becd1c7fa80
Manan Patel <manan77patel@gmail.com>
2019-08-29 17:54:40 +0200

    updated cron job

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { searchForListings } = require("./akelius");
 const cron = require("node-cron");
 
-cron.schedule("* * * *", () => {
+cron.schedule("* */1 * * *", () => {
   console.log(`${new Date().toISOString()} - searching for listings`);
   searchForListings();
 });
