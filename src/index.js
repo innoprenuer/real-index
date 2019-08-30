8ebe95db2f9ca1531e16353c6c1562294ed66cd0
Manan Patel <manan77patel@gmail.com>
2019-08-30 12:46:50 +0200

    added server

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ cron.schedule("* */1 * * *", () => {
   console.log(`${new Date().toISOString()} - searching for listings`);
   searchForListings();
 });
+
+// JUST for heroku
+var http = require("http");
+http
+  .createServer(async function(req, res) {
+    await searchForListings();
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.send("it is running\n");
+  })
+  .listen(process.env.PORT || 5000);
